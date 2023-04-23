a44cd85daa452630729321d6ec10e61837d1ae56
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-04-23 23:52:05 +0300

    corect searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,32 @@
 import css from './Searchbar.module.css';
+import PropTypes from 'prop-types';
 
 const Searchbar = ({ onSubmit }) => {
   return (
     <header className={css.Searchbar}>
-      <form class="form">
-        <button onClick={onSubmit} type="submit" class="button">
-          <span class="button-label">Search</span>
+      <form className={css.SearchForm}>
+        <button
+          onClick={onSubmit}
+          type="submit"
+          className={css.SearchForm__button}
+        >
+          <span className={css.SearchForm__button__label}>Search</span>
         </button>
 
         <input
-          class="input"
+          className={css.SearchForm__input}
           type="text"
-          autocomplete="off"
-          autofocus
+          autoComplete="off"
+          autoFocus
           placeholder="Search images and photos"
         />
       </form>
     </header>
   );
 };
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.bool,
+};
+
 export default Searchbar;
