f50d9d67061fc862a0efee285ea956403c7722ad
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-04-24 21:58:41 +0300

    corect

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,20 @@
 import { Audio } from 'react-loader-spinner';
-<Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="green"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/>;
+
+const Loader = () => {
+  return (
+    <div>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+      ;
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import css from './Modal.module.css';
+
+const Modal = () => {
+  return (
+    <div className={css.Overlay}>
+      <div className={css.Modal}>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+export default Modal;
